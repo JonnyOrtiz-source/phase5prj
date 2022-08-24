459d096567277a2ab3f3f86daf344b10cbc71061
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-08-23 20:22:58 -0400

    wip favorites

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,57 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Nav({ handleCurrentUser, currentUser }) {
+   const [wishlist, setWishlist] = useState([]);
+
+   useEffect(() => {
+      fetch('/wishlists').then((res) => {
+         if (res.ok) {
+            res.json().then((wishlist) => setWishlist(wishlist));
+         } else {
+            console.log('Wishlist not available');
+         }
+      });
+   }, []);
+
+   const history = useHistory();
+
    const logout = () => {
       fetch('/logout', {
          method: 'DELETE',
       }).then(() => {
          handleCurrentUser(null);
       });
+   };
+
+   const handleFaveLink = () => {
+      // if no wish list for the user already, create it
+      if (!currentUser.is_admin && wishlist.length === 0) {
+         const configObj = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+               user_id: currentUser.id,
+            }),
+         };
+         fetch('/wishlists', configObj).then((res) => {
+            if (res.ok) {
+               res.json().then((newWishlist) => {
+                  setWishlist(newWishlist);
+                  history.push('/favorites');
+               });
+            } else {
+               res.json().then(
+                  (data) => console.log(data.errors)
+                  //   setError(data.errors.service_type_name[0])
+                  // data.errors.duration[0]
+                  // data.errors.service_type[0]
+               );
+            }
+         });
+      } else {
+         console.log('wishlist already exists');
+      }
    };
 
    return (
@@ -35,7 +80,11 @@ function Nav({ handleCurrentUser, currentUser }) {
 
             {!currentUser.is_admin && (
                <li className="nav-item">
-                  <Link className="nav-link" to="/favorites">
+                  <Link
+                     className="nav-link"
+                     to="/favorites"
+                     onClick={handleFaveLink}
+                  >
                      Faves
                   </Link>
                </li>
--- client/src/components/favorite/FavoritesList.js
+++ client/src/components/favorite/FavoritesList.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDocumentTitle } from '../../hooks/useDocumentTitle';
+import FavoriteCard from './FavoriteCard';
 
 function FavoritesList() {
-   return <div>Faves</div>;
+   const [favorites, setFavorites] = useState([]);
+
+   useEffect(() => {
+      fetch('/favorites').then((res) => {
+         if (res.ok) {
+            res.json().then((favorite) => setFavorites(favorite));
+         } else {
+            console.log('No favorites');
+         }
+      });
+   }, []);
+
+   useDocumentTitle('Serenity Springs - Favorites List');
+   let favoritesEl;
+
+   if (!favorites.length) {
+      favoritesEl = 'No faves..yet';
+   } else {
+      favoritesEl = favorites.map((favorite) => (
+         <FavoriteCard
+            key={favorite.id}
+            // currentUser={currentUser}
+            favorite={favorite}
+            // updateService={updateService}
+            // deleteService={deleteService}
+            // serviceTypes={serviceTypes}
+            // durations={durations}
+         />
+      ));
+   }
+
+   return <div>{favoritesEl}</div>;
 }
 
 export default FavoritesList;
