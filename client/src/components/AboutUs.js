eaec9dc71bee915e2bfada07cdea382cd0f167ca
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-08-31 15:19:12 -0400

    added pic of the owners

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function AboutUs() {
-   return <div>AboutUs</div>;
+   return (
+      <div className="center">
+         <h2>AboutUs</h2>
+         <img
+            id="owners"
+            src="https://serenity-springs-assets.s3.amazonaws.com/AboutUs.jpeg"
+            alt="Owners' Ariana and Briana"
+         />
+      </div>
+   );
 }
 
 export default AboutUs;
