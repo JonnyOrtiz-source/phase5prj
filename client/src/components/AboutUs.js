04c34a1023506d4ede9ec169e996c489fcd3a82b
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-09-01 09:08:14 -0400

    added about us bio

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -10,6 +10,14 @@ function AboutUs() {
             alt="Owners' Ariana and Briana"
          />
          <p>Owners: Ariana on the left, Briana on the right</p>
+         <h3>
+            Serenity springs is run by sisters Arianna and Brianna Ortiz. They
+            both graduated from Erwin Technical College; Arianna with a license
+            in Cosmetology and Brianna in Massage Therapy. We started this
+            journey because we’ve always loved helping people. Our desire is to
+            help people feel their best from the inside out while offering the
+            cleanest products and services!{' '}
+         </h3>
       </div>
    );
 }
