5f36924ba2256556c89243c29bca86299b68537a
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-09-06 14:26:31 -0400

    improved footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
    return (
-      <div>
-         <footer>
-            Copyright © 2021{' '}
-            <a
-               id="link-cr"
-               href="https://cloudrealmllc.com"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               {' '}
-               Cloud Realm LLC
-            </a>
-         </footer>
-      </div>
+      <footer>
+         <div className="footer-content">
+            <h3>Serenity Springs</h3>
+            <p>
+               <i>live in peace</i>
+            </p>
+            <ul className="socials">
+               <li>
+                  <Link to="#">
+                     <i className="fa-brands fa-facebook"></i>
+                  </Link>
+               </li>
+               <li>
+                  <Link to="#">
+                     <i className="fa-brands fa-twitter"></i>
+                  </Link>
+               </li>
+               <li>
+                  <Link to="#">
+                     <i className="fa-brands fa-google-plus"></i>
+                  </Link>
+               </li>
+               <li>
+                  <Link to="#">
+                     <i className="fa-brands fa-youtube"></i>
+                  </Link>
+               </li>
+               <li>
+                  <Link to="#">
+                     <i className="fa-brands fa-linkedin"></i>
+                  </Link>
+               </li>
+            </ul>
+            <div className="footer-bottom">
+               <div class="footer-menu">
+                  <ul>
+                     <li>
+                        <Link to="/services">Services</Link>
+                     </li>
+                     <li>
+                        <Link to="/aboutus">About</Link>
+                     </li>
+                     <li>
+                        <Link to="#">Contact</Link>
+                     </li>
+                     <li>
+                        <Link to="#">Blog</Link>
+                     </li>
+                  </ul>
+               </div>
+               <p>
+                  Copyright © 2021{' '}
+                  <Link
+                     to="https://cloudrealmllc.com"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                     Cloud Realm LLC
+                  </Link>
+               </p>
+            </div>
+         </div>
+      </footer>
    );
 }
 
